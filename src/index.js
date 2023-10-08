4322cc4f85cea490aea15f08756fec5ba198ba62
your_name <marina.skorik@list.ru>
2023-10-08 13:40:00 +0300

    Отбельный блок меню

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./components/AudioPlayer.css";
+import "./components/NavMenu.css";
 import App from './App';
 import "./App.css";
 
