f462bbb84a25a3bf75bf0abedf84a596f1281bcc
your_name <marina.skorik@list.ru>
2023-10-08 15:49:08 +0300

    ДЗ-2

---
+++ src/components/PlayList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar__item">
+      <a className="sidebar__link" href="#">
+        <img
+          className="sidebar__img"
+          src="img/playlist01.png"
+          alt="day's playlist"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default SideBar;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./components/AudioPlayer.css";
 import "./components/NavMenu.css";
+import "./components/TrackList.css";
+import "./components/TracksBlock.css";
+import "./components/Track.css";
+import "./components/SideBar.css";
+import "./components/PlayList.css";
 import App from './App';
 import "./App.css";
 
