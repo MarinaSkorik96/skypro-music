0cf6c1285689b3a56c5a60fbedcd8700abd3aef3
your_name <marina.skorik@list.ru>
2023-10-11 18:28:37 +0300

    ДЗ 3 первый пункт

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+const { useState } = React;
+
+const BurgerMenu = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  return (
+    <>
+      <div onClick={toggleVisibility} className="nav__burger burger">
+        <span className="burger__line" />
+        <span className="burger__line" />
+        <span className="burger__line" />
+      </div>
+      {visible && (
+        <div className="nav__menu menu">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <a href="#" className="menu__link">
+                Главное
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="#" className="menu__link">
+                Мой плейлист
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="../signin.html" className="menu__link">
+                Войти
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default BurgerMenu;
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BurgerMenu from "./BurgerMenu";
 
 const NavMenu = () => {
   return (
@@ -6,30 +7,7 @@ const NavMenu = () => {
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
-        <span className="burger__line" />
-        <span className="burger__line" />
-        <span className="burger__line" />
-      </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="#" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="../signin.html" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      <BurgerMenu />
     </nav>
   );
 };
