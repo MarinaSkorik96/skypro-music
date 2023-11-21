0cb5087f35549598e7433b6249f2402fc433a85b
your_name <marina.skorik@list.ru>
2023-11-21 22:26:11 +0300

    Не получается

--- src/pages/myplaylist/MyPlaylist.jsx
+++ src/pages/myplaylist/MyPlaylist.jsx
@@ -3,23 +3,23 @@ import { useContext } from 'react';
 import Context from '../../contexts';
 import * as S from "./MyPlayListStyles"
 import TracksBlock from "../../components/TracksBlock/TracksBlock";
-import { useGetFavoritesTracksQuery } from "../../query/tracks";
 import { getFreshToken } from "../../store/slices/user";
 import { useDispatch, useSelector } from "react-redux";
+import { useGetFavoritesTracksQuery } from "../../query/tracks";
 
 
 export const MyPlaylist = () => {
-
+  // const [getFavoritesTracks] = useGetFavoritesTracksQuery();
+  const [getFavoritesTracks] = useGetFavoritesTracksQuery();
   const { setIsMainPage } = useContext(Context)
-  setIsMainPage(false)
+  setIsMainPage(false);
 
-  // const dispatch = useDispatch();
-  // const tokenR = localStorage.getItem('token');
-
-  // dispatch(getFreshToken(tokenR))
-
-  const { data = [], error } = useGetFavoritesTracksQuery()
-  console.log(data)
+  // console.log([useGetFavoritesTracksQuery] )
+  console.log(getFavoritesTracks)
+  const dispatch = useDispatch();
+  const tokenR = localStorage.getItem('token');
+  // const access = useSelector(state =>  state.user.access);
+  dispatch(getFreshToken(tokenR))
 
   return (
     <div>
