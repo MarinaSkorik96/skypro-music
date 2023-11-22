0ea8493fbb25e1d1547bd6ff0e3ef0972f1d03d8
your_name <marina.skorik@list.ru>
2023-11-22 18:57:55 +0300

    Среда

--- src/components/TrackList/MainTrackList.jsx
+++ src/components/TrackList/MainTrackList.jsx
@@ -4,9 +4,15 @@ import FilterButtons from "../FilterButtons/FilterButtons";
 import * as S from "./MainTrackListStyles"
 import { useContext } from 'react';
 import Context from '../../contexts';
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentPage } from "../../store/slices/track";
 
 
 const MainTrackList = () => {
+
+  const dispatch = useDispatch();
+  dispatch(getCurrentPage('main'))
+
   const { setIsMainPage } = useContext(Context)
   setIsMainPage(true)
 
